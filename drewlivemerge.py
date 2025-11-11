008b0b351850a1be76c77edb0b5b89b12e41352c
Bainchiclo <baniknikinchiclo@gmail.com>
2025-11-11 14:59:47 +0800

    Update drewlivemerge.py

--- drewlivemerge.py
+++ drewlivemerge.py
@@ -29,7 +29,7 @@ playlist_urls = [
 ]
 
 EPG_URL = "https://github.com/Drewski2423/DrewLive/raw/refs/heads/main/DrewLive.xml.gz"
-OUTPUT_FILE = "DrewLiveMergedPlaylist.m3u8"
+OUTPUT_FILE = "kibz.m3u8"
 
 def fetch_playlist(url, retries=3, timeout=30):
     headers = {"User-Agent": "Mozilla/5.0"}
