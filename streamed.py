ee226fd7de7596b7652744fc66f8b4a16c996320
Bainchiclo <baniknikinchiclo@gmail.com>
2025-11-25 09:24:02 +0800

    Update streamed.py

--- streamed.py
+++ streamed.py
@@ -18,8 +18,8 @@ logging.getLogger("").addHandler(console)
 log = logging.getLogger("scraper")
 
 CUSTOM_HEADERS = {
-    "Origin": "https://embedsports.top",
-    "Referer": "https://embedsports.top/",
+    "Origin": "https://buffsports.io",
+    "Referer": "https://buffsports.io/",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36"
 }
 
